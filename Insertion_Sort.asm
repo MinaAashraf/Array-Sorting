a0da1606937e1f28a33970fad1e4d97a7471a300
Muhammed Hani <muhammedhani.mh71@gmail.com>
2021-01-02 15:57:05 +0200

    First Commit

---
+++ Insertion_Sort.asm
@@ -0,0 +1,20 @@
+; a method to sort an array using Insertion Sort algorithm..
+.386
+includelib Irvine32
+.model flat, stdcall
+
+ExitProcess proto, dwExitCode: dword
+
+.stack 4096
+
+.data
+
+.code
+main proc
+
+	INVOKE ExitProcess, 0
+main endp
+
+end main
+
+end
