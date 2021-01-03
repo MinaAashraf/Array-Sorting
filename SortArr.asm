3e6e70a534782d8230770b643c950426e5d19f9e
unknown <mohamedalimohamed5050@gmail.com>
2021-01-03 22:58:47 +0200

    First run

--- SortArr.asm
+++ SortArr.asm
@@ -5,35 +5,71 @@ INCLUDE Irvine32.inc
 array SDWORD 10, -1, 5, 11, 32, 4
 outLoop DWORD ?
 inLoop DWORD ?
-msg1 byte "hello from out loop",0
-msg2 byte "hello from inner loop",0
-
+comma BYTE ", ",0
 .code
 main PROC
-MOV EAX, lengthof array
-dec eax 
-;mov outLoop , eax 
-;mov inLoop,0
-mov ecx, 5
+lea ESI, array
+MOV EAX, lengthof array ; get the lenght (n) of array
+dec eax ; eax = n-1 
+mov outLoop , eax ; outloop = n-1 
+mov inLoop,0 
 
+mov ecx, outLoop
 l1:
-push ecx
-lea edx, msg1
-call writeString
-call crlf
+push ecx 
 
-mov ecx,5
+
+
 l2:
-lea edx, msg2
-call writeString
-call crlf
 
-loop l2
+
+;shifting the  inLoop index
+shl inLoop ,2
+MOV EDI,ESI ; EDI = ESI = OFFSET array 
+ADD EDI,inLoop
+
+;shifting OutLoop
+shl outLoop, 2
+MOV EBX,ESI
+ADD EBX,outLoop
+
+; TRANSITION arr[inLoop] = [EDI] , arr[outLoop] = [EBX]
+MOV EAX, [EDI];temp1= EAX = arr[in]
+MOV EDX, [EBX];temp2= EDX = arr[out]
+; after mofifying indexes lets apply condition 
+CMP EDX,EAX ; 
+JGE L3 ; if arr[out] >= arr[int] , then jumb to L3, else complete
+
+; SWapping
+MOV [EBX],EAX; arr[out] = arr[in]
+mov [EDI],EDX; arr[IN] = ARR[out]
+
+inc inLoop
+dec outLoop
+L3: loop l2
 
 
 
 pop cx
+cmp cx,1
+jbe finish 
 loop l1
+finish:
+
+; End of loop 1
+
+; Loop for printing array after sorting 
+MOV ESI,offset array
+MOV ECX,lengthof array
+print:
+MOV EAX,[ESI]
+CALL writeint
+LEA EDX,comma
+CALL writeString
+ADD ESI,4
+loop print
+
+
 call waitmsg
 main ENDP
 END main
