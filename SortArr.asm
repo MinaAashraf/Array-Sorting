24df4c1360d2d9974ce5e041eb7d926adc70f8f7
unknown <mohamedalimohamed5050@gmail.com>
2021-01-04 01:18:48 +0200

    Bubble Sort for entered array before runing the code

--- SortArr.asm
+++ SortArr.asm
@@ -2,56 +2,59 @@
 
 INCLUDE Irvine32.inc
 .data
-array SDWORD 10, -1, 5, 11, 32, 4
+array SDWORD 100, -1, 987, 0, 12, 4
 outLoop DWORD ?
 inLoop DWORD ?
 comma BYTE ", ",0
+msg1 BYTE "The Ascending sort of the array is ",0
 .code
 main PROC
-lea ESI, array
+lea ESI, array ; Load ESI with offset array
 MOV EAX, lengthof array ; get the lenght (n) of array
 dec eax ; eax = n-1 
 mov outLoop , eax ; outloop = n-1 
 mov inLoop,0 
 
-mov ecx, outLoop
+mov ECX, outLoop ; repeat the outer loop for n-1 times 
+; Start Outer Loop
 l1:
-push ecx 
-
-
-
-l2:
-
-
+push ECX 
+; Start Inner Loop
+L2:
 ;shifting the  inLoop index
-shl inLoop ,2
-MOV EDI,ESI ; EDI = ESI = OFFSET array 
-ADD EDI,inLoop
-
-;shifting OutLoop
-shl outLoop, 2
-MOV EBX,ESI
-ADD EBX,outLoop
-
-; TRANSITION arr[inLoop] = [EDI] , arr[outLoop] = [EBX]
-MOV EAX, [EDI];temp1= EAX = arr[in]
-MOV EDX, [EBX];temp2= EDX = arr[out]
-; after mofifying indexes lets apply condition 
-CMP EDX,EAX ; 
-JGE L3 ; if arr[out] >= arr[int] , then jumb to L3, else complete
-
-; SWapping
-MOV [EBX],EAX; arr[out] = arr[in]
-mov [EDI],EDX; arr[IN] = ARR[out]
-
+MOV EDI , inLoop ; EDI = inLoop
+MOV EDX , inLoop ; ECX = inLoop
 inc inLoop
-dec outLoop
-L3: loop l2
+INC EDX			; ECX = inLoop +1 
+SHL EDI,2		; EDI = inLoop *4 
+SHL EDX,2		; ECX = (inLoop+1)*4
+ADD EDI,ESI		; EDI = offset Array + (inLoop)*4 , EDI = &arr [in]
+ADD EDX,ESI		; ECX = offset Array + (inLoop+1)*4, ECX= &arr [in+1]
 
+; getting array values from memory to registers 
+MOV EAX,[EDI]		; EAX = arr [in]
+mov EBX,[EDX]		; EBX = arr [in+1]
 
+; after mofifying indexes lets apply condition 
+CMP EAX,EBX ; 
+JLE L3 ; IF arr[in] <= arr[in+1] jump L3,  else (arr[in]>arr[in+1]) continue
 
-pop cx
-cmp cx,1
+; swapping process 
+MOV [EDI],EBX		; arr[in] = arr[in+1]
+MOV [EDX],EAX		;arr[in+1]=arr[in]
+
+LOOP L2
+jmp next
+
+L3:
+cmp ECX,1
+je next
+loop L2
+next:
+
+mov inLoop,0
+pop ECX
+cmp ECX,1
 jbe finish 
 loop l1
 finish:
@@ -59,7 +62,10 @@ finish:
 ; End of loop 1
 
 ; Loop for printing array after sorting 
-MOV ESI,offset array
+LEA EDX,msg1
+call writeString
+call crlf
+LEA ESI,array
 MOV ECX,lengthof array
 print:
 MOV EAX,[ESI]
@@ -68,7 +74,7 @@ LEA EDX,comma
 CALL writeString
 ADD ESI,4
 loop print
-
+call crlf
 
 call waitmsg
 main ENDP
