a424742a34fb1a839ee107de019574b86d9643ca
Muhammed Hani <muhammedhani.mh71@gmail.com>
2021-01-04 20:39:59 +0200

    insertionSort

--- TEST.asm
+++ TEST.asm
@@ -1,5 +1,4 @@
-; TEST - testing some stuff in assembly..
-; Accessing elements of array..
+; Insertion Sort Algorithm written in Assembly using Irvine32 Library..
 .386
 include Irvine32.inc
 .model flat, stdcall
@@ -7,33 +6,20 @@ ExitProcess proto, dwExitCode: dword
 .stack 4096
 
 .data
-	msg         db ", ", 0
-
-	onebyteArr  db 5, 10, 6, 13, 19
-	twobyteArr  dw 5, 10, 6, 13, 19
-	fourbyteArr dd 5, 10, 6, 13, 19
+	
 
 .code
-main proc
-	mov  ebx, offset fourbyteArr				;saving the memory address of the beginning of the array
-	mov  eax, [ebx]								;eax = first element of the array
-	call writeInt
-	mov  edx, offset msg
-	call writeString
-	mov  eax, [ebx+4]							;eax = second element of the array
-	call writeInt
-	call writeString
-	mov  eax, [ebx+8]							;eax = 3rd element of the array
-	call writeInt
-	call writeString
-	mov  eax, [ebx+12]							;eax = 4th element of the array
-	call writeInt
-	call writeString
-	mov  eax, [ebx+16]							;eax = 5th element of the array
-	call writeInt
-
-	call crlf
+; insertionSort procedure..
+insertionSort proc
 	
+insertionSort endp
+
+main proc
+	
+
+	;just for new line
+	call crlf
+
 	invoke ExitProcess, 0
 main endp
 end main
