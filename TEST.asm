9b0a2bc847653d764904ad976768bee747150dae
Muhammed Hani <muhammedhani.mh71@gmail.com>
2021-01-04 04:56:36 +0200

    Input/Output with Variables

--- TEST.asm
+++ TEST.asm
@@ -1,5 +1,6 @@
 ; TEST - testing some stuff in assembly..
-; First interactive program (accepting input from user and displaying output)..
+; Input/Ouput with variables..
+; This program is to divide two integers..
 .386
 include Irvine32.inc
 .model flat, stdcall
@@ -9,24 +10,53 @@ ExitProcess proto, dwExitCode: dword
 .stack 4096
 
 .data
-	msg1  db "Enter an integer to display..", 0
-	msg2  db "You have entered: ", 0
+	msg1 db "Please, enter two integers: ", 0
+	msg2 db " / ", 0
+	msg3 db " = ", 0
+	msg4 db ", Remainder = ", 0
+
+	num1 dd ?
+	num2 dd ?
+	Q    dd ?
+	R    dd ?
 
 .code
 main proc
-	;Displaying a message to the user in the command line
-	mov  edx, offset msg1				;storing the memory address of variable (msg1) in edx register..
-	call writeString					;displaying the string, stored in edx register, in the command line..
+	mov  edx, offset msg1
+	call writeString					;display a string..
 	call crlf							;just for new line..
 
-	;Accepting input from user
-	call readInt						;accepting signed integer from the user (the entered value is stored in eax register)..
-	
-	;Displaying output for the user in the command line
-	mov  edx, offset msg2				;storing the memory address of variable (msg2) in the edx register..
-	call writeString					;displaying the content (string) of edx register..
-	call writeInt						;displaying the content (signed integer value) of eax register..
-	call crlf							;just a new line..
+	;taking two integers from user..
+	call readInt						;accept the first integer from user..
+	mov  num1, eax						;num1 = eax
+	call readInt						;accept the second integer from user..
+	mov  num2, eax						;num2 = eax
+
+	;division operation..
+	mov  edx, 0							;clear edx
+	mov  eax, num1						;eax = num1
+	mov  ebx, num2						;edx = num2
+	div  ebx							;eax / ebx
+	mov  Q, eax							;Q = eax (Quotient)
+	mov  R, edx							;R = edx (Remainder)
+
+	;Displaying the result..
+	mov  eax, num1
+	call writeInt
+	mov  edx, offset msg2
+	call writeString
+	mov  eax, num2
+	call writeInt
+	mov  edx, offset msg3
+	call writeString
+	mov  eax, Q
+	call writeInt
+	mov  edx, offset msg4
+	call writeString
+	mov  eax, R
+	call writeInt
+
+	call crlf
 
 	INVOKE ExitProcess, 0
 main endp
