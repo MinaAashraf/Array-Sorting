6352eac2fc3ab2b16428ef04524d2569420acbea
mahmoudmohamed22 <mahmoudmohamed5555@yahoo.com>
2021-01-05 23:06:28 +0200

    swap function

--- SortArr.asm
+++ SortArr.asm
@@ -9,11 +9,14 @@ include irvine32.inc ; include labriry
   ;intial variable 
   var1 DWORD 5
   index DWORD 0
+  x_ptr DWORD 8
+  y_ptr DWORD 7
 
 
 .code ; instruction area 
 main proc
- 
+  
+  COMMENT @
  ; accessing fisrt element 
      mov index, offset array 
      mov ecx,3
@@ -26,8 +29,39 @@ main proc
      loop again
      mov [index+4],10
      mov [index+8],10
-     
+     ;swap function 
+     @
 
+
+
+     mov x_ptr,5
+     mov y_ptr,6
+     
+    jmp function_swap
+
+function_swap:
+mov esi,offset x_ptr
+mov eax,[esi]
+mov edi,offset y_ptr   
+mov edx,[edi]
+mov [esi],edx
+mov [edi],eax
+
+
+mov eax,x_ptr
+call writeint ;x_ptr=7
+call crlf 
+mov eax,y_ptr
+call writeint ;x_ptr=8
+
+
+
+call crlf 
+ 
+
+
+
+ COMMENT @
  ;print array
  mov index, offset array 
  mov ecx,3
@@ -44,7 +78,7 @@ main proc
 
  
  
-
+ @
 
   
     
