0e6e86e4dd3398a24ed8946e4a35e206a4545bf7
mahmoudmohamed22 <mahmoudmohamed5555@yahoo.com>
2021-01-05 19:47:48 +0200

    intial array and print

--- SortArr.asm
+++ SortArr.asm
@@ -2,104 +2,49 @@
 include irvine32.inc ; include labriry
 
 .data ; data area // memory area
- array BYTE 3 DUP(?) ; intial array length 3
+ array DWORD 16,2,5 ; intial array length 3
  msg BYTE "enter two variables ?",0 ; message  to user 
  ;shap output string 
- msg1 BYTE "var1 = ",0
- msg2  BYTE "var2 = ",0
- msg3 BYTE "sort 2 number ",0
- msg4 byte "2 variables before swap",0
- msg5 BYTE "2 variables after swap",0
+  
+  ;intial variable 
+  var1 DWORD 5
+  index DWORD 0
 
- ; intial variables 
- var1 DWORD ?
- var2 DWORD ?
- var3 DWORD ?
+
 .code ; instruction area 
 main proc
  
- mov edx,OFFSET msg
- call writestring ;function to write string on screen 
- call crlf
- mov edx,Offset msg1
- call writestring 
- call readint;function to input by user
- mov var1,eax;
-; call crlf ; function to next line 
- mov edx,Offset msg2
- call writestring 
- call readint 
- mov var2,eax 
- ;call crlf
+ ; accessing fisrt element 
+     mov index, offset array 
+     mov ecx,3
+     again:
+     mov eax,var1
+     mov [offset array],eax
+     inc var1
+     add index ,4
+     
+     loop again
+     mov [index+4],10
+     mov [index+8],10
+     
 
- ;sort two variable 
- ;test if condition 
- Mov edx ,offset msg3
- call writestring 
- call crlf
-
- IF
- mov R8D ,var1
- cmp eax ,var2
- jng else
- call writeint 
- call crlf 
- mov eax,var2
+ ;print array
+ mov index, offset array 
+ mov ecx,3
+ again2:
+ mov ebx,index
+ mov eax,[ebx]
+ add index,4
  call writeint 
  call crlf
- jl end
-
- ELSE
- 
- mov eax,var2
- call writeint 
- call crlf
- mov eax ,var1
- call writeint 
- call crlf 
-
-ENDIF
-; print 2 variables before swap 
-mov edx,offset msg4
-call writestring
-call crlf
-
-mov edx,offset msg1
- call writestring 
- mov eax,var1
-call writeint ;function display number to user
-call crlf
- mov edx,offset msg2
- call writestring 
- mov eax,var2
-call writeint 
- call crlf
-
-; swap two variable 
-
-mov eax,var1
-mov var3,eax
-mov edx,var2
-mov var1,edx
-mov var2,eax
+ loop again2
 
 
-  ;print 2 variables
-  mov edx,offset msg5
-call writestring
-call crlf
- mov edx,offset msg1
- call writestring 
- mov eax,var1
-call writeint ;function display number to user
-call crlf
- mov edx,offset msg2
- call writestring 
- mov eax,var2
-call writeint 
- call crlf
+
 
  
+ 
+
 
   
     
@@ -116,7 +61,6 @@ call writeint
 
 
     invoke ExitProcess,0
-   ; .EXIT ; dont work in 32 bit 
-
+   
 main endp
 end main
