06a16f6687467acc6aa5f3c967de5c5e2ec7b091
mahmoudmohamed22 <mahmoudmohamed5555@yahoo.com>
2021-01-07 21:56:46 +0200

    test and run selection sort

--- SortArr.asm
+++ SortArr.asm
@@ -25,45 +25,72 @@ main proc
    dec eax   
    mov OuterCounter,eax
    mov ecx,OuterCounter
-   push ecx
    l1:
-   mov ecx,OuterCounter
     mov index_max,ecx
+    mov var2,ecx
+    mov ecx,OuterCounter
           l2:
           MOV EDI , index_max			; EDI = innerloop
           MOV EDX , ecx            ; ecx=innercounter
         ;  dec ecx ;innerCounter =Innercounter-1
          dec EDX
          SHL EDI,2			; EDI = indexMax *4 
-         SHL EDX,2				    ; ECX = (inLoop-1)*4
+         SHL EDX,2				    ; ECX = (innercounter)*4
          ADD EDI,ESI					; EDI = offset Array + (indexMax)*4 , EDI = &arr [indexMax]
-         ADD EDX,ESI					; ECX = offset Array + (inLoop-1)*4, ECX= &arr [in-1]
+         ADD EDX,ESI					; ECX = offset Array + (innercounter)*4, ECX= &arr [innercounter]
          MOV EAX,[EDI]				; EAX = arr [indexMax]
-         mov EBX,[EDX]				; EBX = arr [innercounter-1]
-         CMP EAX,EBX ;    arr[indexMax] <arr[innercounter-]
-         jl index_maximum
+         mov EBX,[EDX]				; EBX = arr [innercounter] 
+     comment @
+     CMP EBX,EAX ;    arr[indexMax] <arr[innercounter]
+   
+        jb index_maximum
          index_maximum:
          mov ebx   ,ecx
          mov index_max, ebx
+         @
+ 
+    cmp EBX,EAX
+    jng  case
+    jmp en
+
+    case:
+         mov ebx   ,ecx
+         mov index_max, ebx
+
+    en:
+
          loop l2
-     pop ecx
+     mov ecx,var2
      ;swap
+
      mov EDX ,OuterCounter
      SHL EDX,2	
      ADD EDX,ESI            ;edx=&Arr[outercounter]
-     mov EBX,[EDX]
+   
+     MOV   x_ptr,EDX
+     MOV   Y_ptr,EDI
+     mov eax,[x_ptr]
+    mov edx,[y_ptr]
+    mov [x_ptr],edx
+    mov [y_ptr],eax
+
+
+
+    comment @
+    mov EBX,[EDX]
    ;  MOV EBX,var1
      MOV [EDI],EBX				; arr[indexMax] = arr[OuterConter]
      MOV [EDX],EAX				;arr[OuterCounter]=arr[indexMax]
-     LOOP L1
+  @
+ 
+  dec OuterCounter
+   LOOP L1
 
          
 
 
          
-         
-         loop l2
-loop l1
+     
 
 
 
@@ -76,13 +103,15 @@ loop l1
 
      
    
-
+ comment @
 function_swap:
 mov eax,[x_ptr]
 mov edx,[y_ptr]
 mov [x_ptr],edx
 mov [y_ptr],eax
-;jmp backloop1
+jmp back
+
+@
 
 
 
@@ -90,11 +119,11 @@ mov [y_ptr],eax
 
 
 
-
+ 
 
  ;print array
  mov esi, offset array 
- mov ecx,3
+ mov ecx,4
  again2:
  mov eax,[esi]
  add esi,4
